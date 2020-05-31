7ea63d5674f2b4345c2b892aabf3bf3b0175330d
mmcelhan <matthewmcelhaney@gmail.com>
2020-05-31 16:11:00 -0600

    push

--- data_build_scripts/stage_to_warehouse/madden_build.py
+++ data_build_scripts/stage_to_warehouse/madden_build.py
@@ -20,7 +20,6 @@ def main():
 
     df = pd.DataFrame(columns=data['columns'])
 
-
     source_dir = os.path.join(two_up, data['source'])  # should work in both mac and windows
     target_dir = os.path.join(two_up, data['target'])
 
@@ -31,11 +30,10 @@ def main():
         temp_df = pd.read_csv(source)
         temp_df.rename(columns=data['column_rename'], inplace=True)
         temp_df['year'] = data['year'][file['file']]  # add year
-        temp_df['player_match_position'] = temp_df['position'].map(matching['position_groups'])
+        temp_df['position_group'] = temp_df['position'].map(matching['position_groups'])
         temp_df = temp_df[data['columns']]  # cut all extra columns
         new_column_name = str(data['year'][file['file']]) + "_madden_rating"
         temp_df[new_column_name] = temp_df['madden_rating']
-        print(temp_df)
         if counter == 0:
             df = df.append(temp_df)
         else:
--- data_build_scripts/stage_to_warehouse/stage_to_warehouse.py
+++ data_build_scripts/stage_to_warehouse/stage_to_warehouse.py
@@ -1,2 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import combine_stats_build
 import madden_build
+
 
