6b39f8218db08f6b1265fa1444a019ae5823828e
mmcelhan <matthewmcelhaney@gmail.com>
2020-05-30 23:05:48 -0600

    push

--- data_build_scripts/stage_to_warehouse/madden_build.py
+++ data_build_scripts/stage_to_warehouse/madden_build.py
@@ -13,14 +13,13 @@ def make_folder_if_not_exists(path):
 def main():
 
 
-
     local_path = os.path.dirname(os.path.abspath(__file__))
     f = open(os.path.join(local_path, "madden_build.json"))
     data = json.load(f)
 
     two_up = os.path.abspath(os.path.join(local_path, "../.."))
 
-    df = pd.DataFrame(columns = data['columns'])
+    df = pd.DataFrame(columns=data['columns'])
 
 
     source_dir = os.path.join(two_up, data['source'])  # should work in both mac and windows
@@ -38,6 +37,15 @@ def main():
     target_folder = os.path.join(target_dir, data['output_folder'])
     make_folder_if_not_exists(target_folder)
     target = os.path.join(target_folder, data['output_file'])
+    df_players = df = df.pivot_table(
+    values=['madden_rating'],
+    index=['first_name', 'last_name', 'position'],
+    columns='madden_rating'
+    )
+
+# Formatting.
+    df.reset_index(inplace=True)
+    df_players.to_csv("output.csv",index=False)
     df.to_csv(target, index=False)
 
 
