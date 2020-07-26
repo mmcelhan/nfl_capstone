03da64d01ab63017584b5b459f05186fbd104a26
Andrew Morris <andrew.morris@ischool.berkeley.edu>
2020-07-25 20:48:36 -0700

    Updates to site and flask model. (Site changes uploaded already.)
    Need to stop using Flask on localhost.

--- ML/flask/app.py
+++ ML/flask/app.py
@@ -4,7 +4,7 @@
 
 import json
 from flask import Flask, request
-from flask import render_template
+from flask import render_template, make_response
 
 import pickle
 from sklearn.externals import joblib
@@ -28,7 +28,7 @@ def z_list(in_list, cols, stats_df):
     return out_list
 
 
-def get_result(mtype='rb', params=[4.55, 0.66, 214.57, 50.41, 492.14, 40.94, 72.77, 0.60, 19.43, 37.80, 34.36, 7.06, 118.01, 12.74, 70.55]):
+def get_result(mtype, params=[4.55, 0.66, 214.57, 50.41, 492.14, 40.94, 72.77, 0.60, 19.43, 37.80, 34.36, 7.06, 118.01, 12.74, 70.55]):
     '''The main program call.  Returns the category of the expected
     classification.  Defaults are mean params. Input parameters:
     mtype = which position
@@ -41,15 +41,19 @@ def get_result(mtype='rb', params=[4.55, 0.66, 214.57, 50.41, 492.14, 40.94, 72.
     # Load model
     if mtype == 'rb':
         file_name = 'rb_model.pkl'
-    if mtype == 'test':
-        file_name = 'test.pkl'
+        cols = ['rb_forty_zscore','rb_rushing_scrim_tds_pg_cf_scaled_zscore','rb_college_weight_pounds_zscore','rb_rushing_receptions_zscore','rb_rushing_rush_att_zscore','rb_ann_rain_inch_zscore','rb_rushing_scrim_yds_pg_cf_scaled_zscore','rb_rushing_rush_td_pg_cf_scaled_zscore','rb_bench_zscore','rb_rushing_games_zscore','rb_vertical_zscore','rb_threecone_zscore','rb_broad_zscore','rb_rushing_scrim_plays_pg_cf_scaled_zscore','rb_college_height_inches_zscore']
+    if mtype == 'wr':
+        file_name = 'wr_model.pkl'
+        cols = ['wr_forty_zscore', 'wr_vertical_zscore', 'wr_bench_zscore','wr_broad_zscore', 'wr_threecone_zscore', 'wr_shuttle_zscore','wr_receiving_rec_td_pg_cf_scaled_zscore','wr_college_weight_pounds_zscore', 'wr_college_height_inches_zscore','wr_receiving_rec_yards_pg_cf_scaled_zscore','wr_ann_rain_inch_zscore','wr_receiving_receptions_pg_cf_scaled_zscore','wr_receiving_scrim_plays_pg_cf_scaled_zscore']
+    if mtype == 'cb':
+        file_name = 'cb_model.pkl'
+        cols = ['cb_hw_ratio_zscore', 'cb_college_weight_pounds_zscore', 'cb_college_height_inches_zscore', 'cb_forty_zscore', 'cb_vertical_zscore', 'cb_broad_zscore', 'cb_threecone_zscore', 'cb_shuttle_zscore', 'cb_defense_ast_tackles_pg_cf_scaled_zscore', 'cb_defense_fum_forced_pg_cf_scaled_zscore', 'cb_defense_int_pg_cf_scaled_zscore', 'cb_defense_int_td_pg_cf_scaled_zscore', 'cb_defense_int_yards_pg_cf_scaled_zscore', 'cb_defense_loss_tackles_pg_cf_scaled_zscore', 'cb_defense_pd_pg_cf_scaled_zscore', 'cb_defense_solo_tackes_pg_cf_scaled_zscore', 'cb_defense_tackles_pg_cf_scaled_zscore', 'cb_ann_rain_inch_zscore']
     model = joblib.load(file_name)
 
     # Load statistics
     stats_df = pd.read_csv('representative_statistics.csv', index_col = 0)
 
     # prepare argument parameters
-    cols = ['rb_forty_zscore','rb_rushing_scrim_tds_pg_cf_scaled_zscore','rb_college_weight_pounds_zscore','rb_rushing_receptions_zscore','rb_rushing_rush_att_zscore','rb_ann_rain_inch_zscore','rb_rushing_scrim_yds_pg_cf_scaled_zscore','rb_rushing_rush_td_pg_cf_scaled_zscore','rb_bench_zscore','rb_rushing_games_zscore','rb_vertical_zscore','rb_threecone_zscore','rb_broad_zscore','rb_rushing_scrim_plays_pg_cf_scaled_zscore','rb_college_height_inches_zscore']
     eval_df = pd.DataFrame(z_list(params, cols, stats_df)).T
     eval_df.columns = cols
 
@@ -59,37 +63,83 @@ def get_result(mtype='rb', params=[4.55, 0.66, 214.57, 50.41, 492.14, 40.94, 72.
     print("Prediction for this player is:", madden_predict)
     return madden_predict
 
-
 @app.route("/")
 def default_response():
     default_event = {'event_type': 'default'}
     return "This is the default response!\n"
 
-
-@app.route("/query/")
-# Note: currently coded for Running Backs.  New URLs for others.
-# Test with mean: http://127.0.0.1:5000/query/4.55-0.66-214.57-50.41-492.14-40.94-72.77-0.60-19.43-37.80-34.36-7.06-118.01-12.74-70.55
-# Test with star: http://127.0.0.1:5000/query/4.47-1-200-140-600-80-90-1-50-90-90-15-200-15-120
-def query_app():
+@app.route("/query/rb/")
+# Query point for running backs
+# Test with mean: http://127.0.0.1:5000/query/rb/?rb_forty=4.55&rb_rushing_scrim_tds_pg_cf_scaled=0.66&rb_college_weight_pounds=214.57&rb_rushing_receptions=50.41&rb_rushing_rush_att=492.14&rb_ann_rain_inch=40.94&rb_rushing_scrim_yds_pg_cf_scaled=72.77&rb_rushing_rush_td_pg_cf_scaled=0.60&rb_bench=19.43&rb_rushing_games=37.80&rb_vertical=34.36&rb_threecone=7.06&rb_broad=118.01&rb_rushing_scrim_plays_pg_cf_scaled=12.74&rb_college_height_inches=70.55
+def query_rb():
     """Show the model result for given properties"""
-    """Order expected: ..."""
-    posts = []
-    posts.append(float(request.args.get('rb_forty')))
-    posts.append(float(request.args.get('rb_rushing_scrim_tds_pg_cf_scaled')))
-    posts.append(float(request.args.get('rb_college_weight_pounds')))
-    posts.append(float(request.args.get('rb_rushing_receptions')))
-    posts.append(float(request.args.get('rb_rushing_rush_att')))
-    posts.append(float(request.args.get('rb_ann_rain_inch')))
-    posts.append(float(request.args.get('rb_rushing_scrim_yds_pg_cf_scaled')))
-    posts.append(float(request.args.get('rb_rushing_rush_td_pg_cf_scaled')))
-    posts.append(float(request.args.get('rb_bench')))
-    posts.append(float(request.args.get('rb_rushing_games')))
-    posts.append(float(request.args.get('rb_vertical')))
-    posts.append(float(request.args.get('rb_threecone')))
-    posts.append(float(request.args.get('rb_broad')))
-    posts.append(float(request.args.get('rb_rushing_scrim_plays_pg_cf_scaled')))
-    posts.append(float(request.args.get('rb_college_height_inches')))
+    """Order expected is listed below."""
+    pargs = []
+    pargs.append(float(request.args.get('rb_forty')))
+    pargs.append(float(request.args.get('rb_rushing_scrim_tds_pg_cf_scaled')))
+    pargs.append(float(request.args.get('rb_college_weight_pounds')))
+    pargs.append(float(request.args.get('rb_rushing_receptions')))
+    pargs.append(float(request.args.get('rb_rushing_rush_att')))
+    pargs.append(float(request.args.get('rb_ann_rain_inch')))
+    pargs.append(float(request.args.get('rb_rushing_scrim_yds_pg_cf_scaled')))
+    pargs.append(float(request.args.get('rb_rushing_rush_td_pg_cf_scaled')))
+    pargs.append(float(request.args.get('rb_bench')))
+    pargs.append(float(request.args.get('rb_rushing_games')))
+    pargs.append(float(request.args.get('rb_vertical')))
+    pargs.append(float(request.args.get('rb_threecone')))
+    pargs.append(float(request.args.get('rb_broad')))
+    pargs.append(float(request.args.get('rb_rushing_scrim_plays_pg_cf_scaled')))
+    pargs.append(float(request.args.get('rb_college_height_inches')))
+    val = get_result(mtype='rb', params=pargs)
+    return render_template('/query/index.html', ret_value=val)
 
-    val = get_result(mtype='rb', params=posts)
-    # Research this part at https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-    return render_template('/query/index.html', posts=posts)
+@app.route("/query/wr/")
+# Query point for wide receivers
+# Test with mean:
+def query_wr():
+    """Show the model result for given properties"""
+    """Order expected is listed below."""
+    pargs = []
+    pargs.append(float(request.args.get('wr_forty')))
+    pargs.append(float(request.args.get('wr_vertical')))
+    pargs.append(float(request.args.get('wr_bench')))
+    pargs.append(float(request.args.get('wr_broad')))
+    pargs.append(float(request.args.get('wr_threecone')))
+    pargs.append(float(request.args.get('wr_shuttle')))
+    pargs.append(float(request.args.get('wr_receiving_rec_td_pg_cf_scaled')))
+    pargs.append(float(request.args.get('wr_college_weight_pounds')))
+    pargs.append(float(request.args.get('wr_college_height_inches')))
+    pargs.append(float(request.args.get('wr_receiving_rec_yards_pg_cf_scaled')))
+    pargs.append(float(request.args.get('wr_ann_rain_inch')))
+    pargs.append(float(request.args.get('wr_receiving_receptions_pg_cf_scaled')))
+    pargs.append(float(request.args.get('wr_receiving_scrim_plays_pg_cf_scaled')))
+    val = get_result(mtype='wr', params=pargs)
+    return render_template('/query/index.html', ret_value=val)
+
+@app.route("/query/cb/")
+# Query point for corner backs
+# Test with mean:
+def query_cb():
+    """Show the model result for given properties"""
+    """Order expected is listed below."""
+    pargs = []
+    pargs.append(float(request.args.get('cb_hw_ratio')))
+    pargs.append(float(request.args.get('cb_college_weight_pounds')))
+    pargs.append(float(request.args.get('cb_college_height_inches')))
+    pargs.append(float(request.args.get('cb_forty')))
+    pargs.append(float(request.args.get('cb_vertical')))
+    pargs.append(float(request.args.get('cb_broad')))
+    pargs.append(float(request.args.get('cb_threecone')))
+    pargs.append(float(request.args.get('cb_shuttle')))
+    pargs.append(float(request.args.get('cb_defense_ast_tackles_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_fum_forced_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_int_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_int_td_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_int_yards_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_loss_tackles_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_pd_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_solo_tackes_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_defense_tackles_pg_cf_scaled')))
+    pargs.append(float(request.args.get('cb_ann_rain_inch')))
+    val = get_result(mtype='cb', params=pargs)
+    return render_template('/query/index.html', ret_value=val)
